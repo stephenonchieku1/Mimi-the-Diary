b187474b4cee30d21db2f2e28704cafffd618c62
stephenonchieku1 <110173070+stephenonchieku1@users.noreply.github.com>
2022-09-30 08:54:15 +0300

    done

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -5,45 +5,52 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 function AccountContainer() {
-  const[transactions,setTransactions]=useState([]);
+  const [isGetLoading, setIsGetLoading] = useState(false);
+  const [isSendLoading, setIsSendLoading] = useState(false);
+  const [transactions, setTransactions] = useState([]);
+
   useEffect(() => {
-    fetch("http://localhost:8001/transactions")
-      .then((response) => response.json())
-      .then((data) => {
-        setTransactions(data);
-      });
+    const getTransactionsHandler = async  () => {
+      setIsGetLoading(true);
+      const response = await fetch('http://localhost:8001/transactions');
+      const responseData = await response.json();
+      setTransactions(responseData);
+      setIsGetLoading(false);
+    };
+    getTransactionsHandler();
   }, []);
 
-  function handleFormSubmission(newtransactioninput){
-    console.log(newtransactioninput)
-    setTransactions((transactions)=>[...transactions,newtransactioninput])
-    const configurationData =  {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newtransactioninput),
+  useEffect(() => {
+    setTransactions(transactions);
+  }, [transactions]);
+
+  const searchHandler = async (input) => {
+    setTransactions(transactions.filter((each) => each.description.toLowerCase().includes(input)));
+
+  }
+
+
+  const addTransactionHandler = async (date, description, category, amount) => {
+    try {
+      setIsSendLoading(true);
+      const response = await fetch('http://localhost:8001/transactions', { method: 'POST', headers: { 'Content-Type': 'application/json', 'accept': 'application/json' }, body: JSON.stringify({ date, description, category, amount: parseInt(amount) }) });
+      const responseData = await response.json();
+      setTransactions(prevTransactions => [...prevTransactions, responseData]);
+    } catch(err) {
+      console.log(err);
+    } finally {
+      setIsSendLoading(false);
     }
-    fetch("http://localhost:8001/transactions",configurationData)
-     .then((response)=>response.json())
-       .then(newItemTRansacted=>setTransactions(transactions=>[...transactions,newItemTRansacted]))
-         .catch((error)=>{console.log(error)})
-}
+  }
 
-function handleSearch(search){
-   console.log(search)
-   const filterSEarch = transactions.filter((transaction)=>{return transaction.description.toLowerCase().includes(search.toLowerCase()) })
-   setTransactions(filterSEarch)
-   console.log(transactions)
-}
-
-    return (
+  return (
     <div>
-      <Search whenSearched={handleSearch}/>
-      <AddTransactionForm  onSubmission={handleFormSubmission}/>
-      <TransactionsList  transactions={transactions}/>
+      <Search searchHandler={searchHandler} />
+      <AddTransactionForm isSendLoading={isSendLoading} addTransactionHandler={addTransactionHandler} />
+      <TransactionsList isGetLoading={isGetLoading} transactions={transactions} />
     </div>
   );
 }
 
 export default AccountContainer;
+  
